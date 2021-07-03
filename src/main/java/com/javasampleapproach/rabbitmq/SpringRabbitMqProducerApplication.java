e8bfdca156a7a4560b6418b9f66fb84f804b3dd3
KimTaeYeong <primesecret@naver.com>
2021-07-03 19:35:44 +0900

    commit test

--- src/main/java/com/javasampleapproach/rabbitmq/SpringRabbitMqProducerApplication.java
+++ src/main/java/com/javasampleapproach/rabbitmq/SpringRabbitMqProducerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRabbitMqProducerApplication {
 
+	// main comment
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRabbitMqProducerApplication.class, args);
 	}
